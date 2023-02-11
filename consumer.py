5e0ea560846867435526aa67f55300992a750b11
shoumitro-cse <shoumitro26@gmail.com>
2023-02-11 16:30:51 +0600

    kafka consumer info updated

--- consumer.py
+++ consumer.py
@@ -6,24 +6,28 @@ import json
 #bootstrap_servers=['localhost:39092'] # works fine for kafka cluster(single node connection) 
 #bootstrap_servers=['localhost:29092'] # works fine for kafka cluster(single node connection) 
 
-
 bootstrap_servers=['localhost:29092', 'localhost:39092'] # works fine for kafka cluster(load balancing) 
 	    
 	    
 # To consume latest messages and auto-commit offsets
-consumer = KafkaConsumer('my-topic',
-                         group_id='my-group',
-                         bootstrap_servers=bootstrap_servers)
-
+consumer = KafkaConsumer('my-topic', group_id='my-group', bootstrap_servers=bootstrap_servers)
 
 for message in consumer:
     # message value and key are raw bytes -- decode if necessary!
     # e.g., for unicode: `message.value.decode('utf-8')`
-    print ("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition,
-                                          message.offset, message.key,
-                                          message.value))
+    
+    #print(message)
+    # ConsumerRecord(topic='my-topic', partition=0, offset=870, timestamp=1676109866735, timestamp_type=0, 
+    #                key=None, value=b'raw_bytes', headers=[], checksum=None, serialized_key_size=-1, 
+    #                serialized_value_size=9, serialized_header_size=-1)
 
-print("Here!")
+    #print(message.value.decode('utf-8')) # {"key": 9} of message.value
+    
+    print ("topic=%s partition=%d offset=%d: key=%s value=%s" % (
+          message.topic, message.partition, message.offset, message.key, message.value)
+    )
+
+print("Will not reach here any time!")
 
 # consume earliest available messages, don't commit offsets
 consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest', enable_auto_commit=False)
@@ -43,18 +47,15 @@ consumer = KafkaConsumer(consumer_timeout_ms=1000, bootstrap_servers=bootstrap_s
 
 # Subscribe to a regex topic pattern
 consumer = KafkaConsumer(bootstrap_servers=bootstrap_servers)
-consumer.subscribe(pattern='^awesome.*')
+consumer.subscribe(pattern='^my-topi.*')
 
 
 # Use multiple consumers in parallel w/ 0.9 kafka brokers
 # typically you would run each on a different server / process / CPU
-consumer1 = KafkaConsumer('my-topic',
-                          group_id='my-group',
-                          bootstrap_servers=bootstrap_servers)
-                          
-consumer2 = KafkaConsumer('my-topic',
-                          group_id='my-group',
-                          bootstrap_servers=bootstrap_servers)
+consumer1 = KafkaConsumer('my-topic', group_id='my-group', bootstrap_servers=bootstrap_servers)                          
+consumer2 = KafkaConsumer('my-topic', group_id='my-group', bootstrap_servers=bootstrap_servers)
+
+
                           
 
 
